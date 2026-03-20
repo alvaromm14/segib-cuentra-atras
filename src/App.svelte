fc48ad99db9460c19c79f06e179c9ebbc0c7b118
alvaromm14 <49958868+alvaromm14@users.noreply.github.com>
2026-03-20 20:33:31 +0100

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -82,7 +82,7 @@
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
-      width: 90%;
+      width: 100%;
       text-align: center;
       z-index: 10;
     }
@@ -106,9 +106,5 @@
     .contador-wrapper :global(.line2) {
       font-size: 1rem;
     }
-  }
-
-  :global(body) {
-    background: #f3f7fb;
   }
 </style>
